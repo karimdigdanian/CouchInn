b0f9bf5bc5df004028248caeb1f38110210c9332
karimdigdanian <karim.lp@hotmail.com>
2016-06-20 09:48:43 -1200

    adf2

---
+++ ProyectoIngSoft/ProyectoIngSoft/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ProyectoIngSoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucCuerpo1 = new libCounchinn.Controles.ucCuerpo();
+            this.SuspendLayout();
+            // 
+            // ucCuerpo1
+            // 
+            this.ucCuerpo1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ucCuerpo1.Location = new System.Drawing.Point(29, 18);
+            this.ucCuerpo1.Modo = libCounchinn.Clases.Modo_Pantalla.NoLogeado;
+            this.ucCuerpo1.Name = "ucCuerpo1";
+            this.ucCuerpo1.Size = new System.Drawing.Size(924, 644);
+            this.ucCuerpo1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 557);
+            this.Controls.Add(this.ucCuerpo1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
+            this.Style = MetroFramework.MetroColorStyle.Lime;
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private libCounchinn.Controles.ucCuerpo ucCuerpo1;
+    }
+}
+
---
+++ ProyectoIngSoft/libCounchinn/Controles/ucOlvideContraseña.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libCounchinn.Clases;
+using libCounchinn.Modelo;
+
+namespace libCounchinn.Controles
+{
+    public partial class ucOlvideContraseña : UserControl
+    {
+        #region Variables
+
+        public event DelegadoLogin Volver;
+
+        private ModeloLibCouchin varModelo;
+
+        #endregion
+
+        #region Inicializar
+
+        public ucOlvideContraseña()
+        {
+            InitializeComponent();
+        }
+
+        public void Inicilizar()
+        {
+            this.varModelo = new ModeloLibCouchin(ClaseDeConfiguracion.EFConnectionString);
+        }
+
+        #endregion
+
+        #region Eventos Privados
+
+        private void mbtEnviarNuevaContreseña_Click(object sender, EventArgs e)
+        {
+            string StEmail = this.varModelo.SelectEmailUsuario(this.mTxtOlvide.Text);
+            if (StEmail != null && StEmail != "Usuario Bloqueado")
+            {
+                #region Generacion Nueva Contraseña
+                Random obj = new Random();
+                string posibles = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                int longitud = posibles.Length;
+                char letra;
+                int longitudnuevacadena = 8;
+                string nuevacadena = "";
+                for (int i = 0; i < longitudnuevacadena; i++)
+                {
+                    letra = posibles[obj.Next(longitud)];
+                    nuevacadena += letra.ToString();
+                }
+                #endregion
+                libcCouchin.Clases.Correo CorreoRecuperacion = new libcCouchin.Clases.Correo();
+                CorreoRecuperacion.enviarCorreo(nuevacadena, StEmail);
+                USUARIO us = varModelo.SelectUsuarioxNombreUsuario(this.mTxtOlvide.Text);
+                this.bindingSourceActualizaContraseña.DataSource = this.varModelo.UPD_ACTUALIZAR_CONTRASENA(us.ID_USUARIO, nuevacadena);
+                
+            }
+            else
+            {
+                if (StEmail != null)
+                {
+                    MessageBox.Show("El usuario ingresado se encuentra bloquedo", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("El usuario ingresado no es valido", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        #endregion
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            if (this.Volver != null)
+            {
+                this.Visible = false;
+                this.SendToBack();
+                this.Volver();
+            }
+        }
+    }
+}
